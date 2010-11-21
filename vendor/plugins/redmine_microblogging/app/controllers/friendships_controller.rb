b9648662f53864fffe732eac8feb365006628ae6
Nuno Polonia <nunopolonia@gmail.com>
2010-11-21 20:10:25 +0000

    changed redirects and flash messages

--- vendor/plugins/redmine_microblogging/app/controllers/friendships_controller.rb
+++ vendor/plugins/redmine_microblogging/app/controllers/friendships_controller.rb
@@ -5,30 +5,30 @@ class FriendshipsController < ApplicationController
     @friendship = find_current_user.friendships.build(:friend_id => params[:friend_id])
     if params[:friend_id] == find_current_user.id
       flash[:error] = "Can't follow yourself."
-      redirect_to root_url
+      redirect_to posts_url
     end
     if @friendship.save
-      flash[:notice] = "Added friend."
-      redirect_to root_url
+      flash[:notice] = "Following request sent."
+      redirect_to posts_url
     else
-      flash[:error] = "Unable to add friend."
-      redirect_to root_url
+      flash[:error] = "Error sending following request."
+      redirect_to posts_url
     end
   end
 
   def destroy
     @friendship = find_current_user.friendships.find(params[:id])
     @friendship.destroy
-    flash[:notice] = "Removed friendship."
-    redirect_to root_url
+    flash[:notice] = "Stopped following user."
+    redirect_to posts_url
   end
 
   def accept
     @friendship = find_current_user.inverse_friendships.find(params[:id])
     @friendship.accepted = true
     @friendship.save
-    flash[:notice] = "Accepted friendship."
-    redirect_to root_url
+    flash[:notice] = "Following request accepted."
+    redirect_to posts_url
   end
 
   def block
@@ -36,8 +36,8 @@ class FriendshipsController < ApplicationController
     @friendship.accepted = false
     @friendship.blocked = true
     @friendship.save
-    flash[:notice] = "Blocked friendship."
-    redirect_to root_url
+    flash[:notice] = "User sucessfuly blocked."
+    redirect_to posts_url
   end
 end
 
