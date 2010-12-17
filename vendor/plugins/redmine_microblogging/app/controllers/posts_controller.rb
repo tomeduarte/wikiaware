99d0ec6797b894d4859c3a754f057ce4254c8dfc
Tomé Duarte <tome.duarte@gmail.com>
2010-12-17 16:52:58 +0000

    Removed XML responses from PostsController

--- vendor/plugins/redmine_microblogging/app/controllers/posts_controller.rb
+++ vendor/plugins/redmine_microblogging/app/controllers/posts_controller.rb
@@ -3,7 +3,6 @@ class PostsController < ApplicationController
   before_filter :require_login
 
   # GET /posts
-  # GET /posts.xml
   def index
     @user = find_current_user
     @search_results = User.search(params[:search])
@@ -15,56 +14,34 @@ class PostsController < ApplicationController
     end
     @user.posts.each do |p| timeline << {:user => @user, :post => p} end
     @timeline = timeline.sort_by{ |line| -line[:post].created_at.to_i }
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @posts }
-    end
   end
 
   # GET /posts/new
-  # GET /posts/new.xml
   def new
     @post = Post.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @post }
-    end
   end
 
   # POST /posts
-  # POST /posts.xml
   def create
     @post = Post.new(params[:post])
     @post.user = find_current_user
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to(posts_url, :notice => 'Post was successfully created.') }
-        format.xml  { render :xml => @post, :status => :created, :location => @post }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
-      end
+    if @post.save
+      redirect_to(posts_url, :notice => 'Post was successfully created.')
+    else
+      render :action => "new"
     end
   end
 
   # DELETE /posts/1
-  # DELETE /posts/1.xml
   def destroy
     begin
-        @post = find_current_user.posts.find(params[:id])
-        @post.destroy
+      @post = find_current_user.posts.find(params[:id])
+      @post.destroy
 
-        respond_to do |format|
-          format.html { redirect_to(posts_url, :notice => 'Post was successfully deleted.') }
-          format.xml  { head :ok }
-        end
+      redirect_to(posts_url, :notice => 'Post was successfully deleted.')
     rescue ActiveRecord::RecordNotFound
-        respond_to do |format|
-            format.html { redirect_to(posts_url, :error => 'Unable to delete post.') }
-        end
+      redirect_to(posts_url, :error => 'Unable to delete post.')
     end
   end
 end
