d75d33108dda0d5d09a8af37e158535eaaaa98cd
Tomé Duarte <tome.duarte@gmail.com>
2010-12-17 16:51:49 +0000

    Fixed routes for posts - not all RESTful routes are used.

--- vendor/plugins/redmine_microblogging/config/routes.rb
+++ vendor/plugins/redmine_microblogging/config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :posts
+  map.resources :posts, :only => [:index, :new, :create, :destroy]
   map.resources :friendships,
     :member => { :accept => :get, :block => :get, :reject => :get }
   map.resources :subscriptions, :only => [:index, :create, :destroy]
