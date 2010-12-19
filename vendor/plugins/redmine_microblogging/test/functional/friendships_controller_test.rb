b6f6d9a02e73331ca7f5e55a246b2c9cd50db003
Nuno Polonia <nunopolonia@gmail.com>
2010-12-19 22:59:33 +0000

    bugfixes on friendships tests

--- vendor/plugins/redmine_microblogging/test/functional/friendships_controller_test.rb
+++ vendor/plugins/redmine_microblogging/test/functional/friendships_controller_test.rb
@@ -15,18 +15,28 @@ class FriendshipsControllerTest < ActionController::TestCase
   context "routes" do
     should_route  :post, '/friendships', :controller => 'friendships', :action => 'create'
     should_route  :delete, '/friendships/1', :controller => 'friendships', :action => 'destroy', :id => 1
-    should_route  :get, '/friendships/1/block', :controller => 'friendships', :action => 'block_friendship', :id => 1
-    should_route  :get, '/friendships/1/accept', :controller => 'friendships', :action => 'accept_friendship', :id => 1
-    should_route  :get, '/friendships/1/reject', :controller => 'friendships', :action => 'reject_friendship', :id => 1
+    should_route  :get, '/friendships/1/block', :controller => 'friendships', :action => 'block', :id => 1
+    should_route  :get, '/friendships/1/accept', :controller => 'friendships', :action => 'accept', :id => 1
+    should_route  :get, '/friendships/1/reject', :controller => 'friendships', :action => 'reject', :id => 1
   end
 
-  context "post :create" do
+  context "post :create can't add yourself" do
     setup do
-      post :create, :friendship => {:friend_id => '2'}
+      post :create, :friend_id => '1'
     end
 
     should_respond_with :redirect
-    should_redirect_to("index") {{:controller => 'posts', :action => 'index'}}
+    should_redirect_to("index") { posts_url }
+  end
+
+
+  context "post :create" do
+    setup do
+      post :create, :friend_id => '2'
+    end
+
+    should_respond_with :redirect
+    should_redirect_to("index") { posts_url }
   end
 
   context "delete :destroy" do
@@ -36,7 +46,7 @@ class FriendshipsControllerTest < ActionController::TestCase
       end
 
       should_respond_with :redirect
-      should_redirect_to("index") {{:controller => 'posts', :action => 'index'}}
+      should_redirect_to("index") { posts_url }
   end
 
 end
